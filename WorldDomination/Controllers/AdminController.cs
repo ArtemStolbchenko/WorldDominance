dab657ebc73e8ca4bedd0fd26c1e61f4605e5f23
Stolbchenko,Artem A.S. <a.stolbchenko@student.fontys.nl>
2023-12-25 22:38:09 +0100

    added JWT generation and admin password verification

---
+++ WorldDomination/Controllers/AdminController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using WorldDomination.Databases;
+using WorldDomination.Helpers;
+using WorldDomination.Models;
+
+namespace WorldDomination.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AdminController : ControllerBase
+    {
+        private readonly AppDbContext _appDbContext;
+        private readonly ILogger<CodeController> _logger;
+        private readonly IConfiguration _config;
+        public AdminController(ILogger<CodeController> logger, AppDbContext dbContext, IConfiguration config)
+        {
+            _logger = logger;
+            _appDbContext = dbContext;
+            _config = config;
+        }
+        [HttpPost]
+        public IActionResult Login([FromBody] string password)
+        {
+
+            if (password == _config["Admin:Password"])
+            {
+
+                //If login usrename and password are correct then proceed to generate token
+
+                var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["JwtSettings:SecretKey"]));
+                var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+                var Sectoken = new JwtSecurityToken(_config["JwtSettings:Issuer"],
+                  _config["JwtSettings:Issuer"],
+                  null,
+                  expires: DateTime.Now.AddMinutes(120),
+                  signingCredentials: credentials);
+
+                var token = new JwtSecurityTokenHandler().WriteToken(Sectoken);
+
+                return Ok(token);
+            }
+            else return BadRequest("Wrong password, gtfo!");
+        }
+    }
+}
--- WorldDomination/Program.cs
+++ WorldDomination/Program.cs
@@ -1,7 +1,38 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 using WorldDomination.Databases;
+using WorldDomination.Models;
 
 var builder = WebApplication.CreateBuilder(args);
+//at first start, remember to dotnet user-secrets set "JwtSettings:SecretKey" "*your key*"
+//same for "Admin:Password"
+
+
+//Configuring JWT
+
+var jwtIssuer = builder.Configuration.GetSection("JwtSettings:Issuer").Get<string>();
+var jwtKey = builder.Configuration["JwtSettings:SecretKey"];
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+ .AddJwtBearer(options =>
+ {
+     options.TokenValidationParameters = new TokenValidationParameters
+     {
+         ValidateIssuer = true,
+         ValidateAudience = true,
+         ValidateLifetime = true,
+         ValidateIssuerSigningKey = true,
+         ValidIssuer = jwtIssuer,
+         ValidAudience = jwtIssuer,
+         IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey))
+     };
+ });
+
+// Load administrator password
+
+builder.Services.AddSingleton<IConfiguration>(builder.Configuration);
 
 // Add services to the container.
 
@@ -35,8 +66,9 @@ if (app.Environment.IsDevelopment())
 
 //app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
-app.Urls.Add("http://26.145.106.179:7281");
+//app.Urls.Add("http://26.145.106.179:7281");
 app.UseCors("default"); 
 
 app.MapControllers();
