a10c098220d28a27fa5507a70758cb37f880f4e1
Stolbchenko,Artem A.S. <a.stolbchenko@student.fontys.nl>
2023-12-26 17:54:43 +0100

    added a sample auth test function

--- WorldDomination/Controllers/AdminController.cs
+++ WorldDomination/Controllers/AdminController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Text;
@@ -44,6 +45,13 @@ namespace WorldDomination.Controllers
                 return Ok(token);
             }
             else return BadRequest("Wrong password, gtfo!");
+        }
+
+        [HttpGet]
+        [Authorize]
+        public IActionResult TestAuth()
+        {
+            return Ok("перемога");
         }
     }
 }
